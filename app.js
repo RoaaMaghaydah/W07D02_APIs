c0dd740442e535c46b7031b57bc96c65ab6dfbc9
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-10 18:54:20 +0300

    api practice q4

--- app.js
+++ app.js
@@ -4,38 +4,34 @@ const port = 5000;
 
 app.use(express.json());
 
-const todos = [{ todo: " wake up", isCompleted: false },
+const todos = [{ todo: "wake up", isCompleted: false },
  { todo: "Eat Breakfast", isCompleted: false }];
-
 
 
  app.get("/todos",(req,res)=>{
      res.json(todos);
  })
 
- app.post("/create/todo",(req,res)=>{
+ app.post("/create/todo",(req,res)=>{   
      const newtodo={todo:req.body.todo,isCompleted:req.body.isCompleted};
      todos.push(newtodo);
      res.status(201);
      res.json(newtodo);
  })
 
- app.put("/update/todo/:todo",(req,res)=>{
-     const todoo=req.params.todo;
-     const found = todos.find((element) => {
-        return element.todo ===todoo;
-      });
-    
-      if (found) {
-        
-        res.status(200);
-        element.todo=`${element.todo}uddate`;
-        res.json(element.todo);
-      } else {
-        // set the response status code to 404 (Not Found)
-        res.status(404);
-        res.json("User not found");
-      }
+    app.put("/update/todo/:todo",(req,res)=>{
+        const todoo=req.params.todo;
+        console.log(todoo);
+         todos.find((element,index) => {
+             console.log(element.todo);
+             console.log(element.todo === todoo)
+             if(element.todo === todoo){
+            
+             todos[index].todo=req.body.todo;
+             }
+          });
+           res.json(todos)    
+     
     });
 
     
